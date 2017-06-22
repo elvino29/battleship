37de3a7f4f4d2f6738990e239e1993fb1d9d1320
elvino 29 <kevindoh26@gmail.com>
2017-06-23 00:23:59 +0200

    srver mise  jour à nouveau

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // express
+path = require('path');
 var express = require('express');
 
 // handlebars
-var handlebars = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 // socket io
 app = express();
 
 http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname + '/')));
+
+app.set('views', __dirname + '/src/Views');
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/src/Views/layouts'}));
+app.set('view engine', 'handlebars');
 
 // import models
 // models = require('./src/Models');
